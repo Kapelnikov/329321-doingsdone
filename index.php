2ecfcacfad8a54f43f54ffc495319589e0941e1d
Konstantin Kapelnikov <reg_kapelnikovka@mail.ru>
2017-12-01 18:23:13 +0400

    Валидация формы

--- index.php
+++ index.php
@@ -28,14 +28,93 @@ if (isset($_GET['project_id'])) {
 }
 
 
+
+$modal = '';
+$overlay = '';
+if (isset($_GET['add'])) {
+    $overlay = "overlay";
+   
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $new_tasks = [
+            "Задача" => $_POST["name"],
+            "Дата выполнения" => $_POST["date"],
+            "Категория" => $_POST["project"],
+            ];
+    array_unshift($tasks, $new_tasks);
+    array_unshift($filtered_tasks, $new_tasks);
+}
+   
+$errors = [];   
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $new_posts = $_POST;
+
+    $required = ['name', 'project'];
+    $dict = ['name' => 'Задача', 'project' => 'Проект'];
+    $errors = [];
+    $data = $_POST;
+
+    foreach ($_POST as $key => $value) {
+        if (in_array($key, $required)) {
+            if (!$value or empty($value)) {
+                $errors[$key] = 'Это поле надо заполнить';
+            }
+        }
+
+    }
+
+    if (!empty($_FILES) and isset ($_FILES["preview"])) {
+
+
+        $tmp_name = $_FILES['preview']['tmp_name'];
+        $path = $_FILES['preview']['name'];
+        
+
+        move_uploaded_file($tmp_name, './uploads/' . $path);
+    }
+
+if (!empty($errors)) {
+        $page_content = renderTemplate($config['templates_path'] . 'modal.php', ['projects' => $projects, 'errors' => $errors, 'data' => $data]);
+        echo $page_content;
+    }
+
+else {$overlay = "";}
+
+}
+
+else {
+
+    $modal = require_once(__DIR__.'../templates/modal.php');
+}
+
+
+
+   
+
+}
+
+if (isset($_GET['close'])) {
+    !$modal;
+    }
+
+
+
+
 $page_content = renderTemplate($config['templates_path'] . 'index.php', ['projects' => $projects, 'tasks' => $tasks, 'filtered_tasks' => $filtered_tasks]);
 
 if ($config['enable']) {
-    $layout_content = renderTemplate($config['templates_path'] . 'layout.php', ['projects' => $projects, 'tasks' => $tasks, 'content' => $page_content, 'page_title' => 'Дела в порядке', 'filtered_tasks' => $filtered_tasks]);
+    $layout_content = renderTemplate($config['templates_path'] . 'layout.php', ['projects' => $projects, 'tasks' => $tasks, 'content' => $page_content, 'page_title' => 'Дела в порядке', 'filtered_tasks' => $filtered_tasks, 'overlay' => $overlay, 'modal' => $modal]);
 } else {
-    $layout_content = renderTemplate($config['templates_path'] . 'off.php', ['projects' => $projects, 'tasks' => $tasks, 'error_msg' => 'Сорямба гайз, сайт не работает!', 'page_title' => 'Дела в порядке', 'filtered_tasks' => $filtered_tasks]);
+    $layout_content = renderTemplate($config['templates_path'] . 'off.php', ['projects' => $projects, 'tasks' => $tasks, 'error_msg' => 'Сорямба гайз, сайт не работает!', 'page_title' => 'Дела в порядке', 'filtered_tasks' => $filtered_tasks, 'overlay' => $overlay, 'modal' => $modal]);
+
+
+
 }
 
 print($layout_content);
 
 ?>
+
+
+
+<!--Скрипт LiveReload-->
+    <script>document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')</script>
---
+++ templates/modal.php
@@ -0,0 +1,86 @@
+  
+
+<?php
+$task_name = $_POST["name"] ?? "";
+$task_date = $_POST["date"] ?? "";
+
+
+
+
+
+
+?>
+
+
+
+  <div class="modal">
+    
+    <a href="index.php?close"><button class="modal__close" type="button" name="button">Закрыть</button></a>
+    <h2 class="modal__heading">Добавление задачи</h2>
+
+    <form class="form"  action="index.php?add" method="post" enctype="multipart/form-data">
+     
+      <div class="form__row">
+        <label class="form__label" for="name">Название <sup>*</sup></label>
+
+        <input class="form__input <?php if (isset($errors['name'])) echo 'form__input--error';?>" type="text" name="name" id="name" value="<?=$task_name;?>" placeholder="Введите название">
+
+        <?php if (isset($errors['name'])): ?> 
+            <p class="p.form__message"><?php echo $errors["name"]?></p>
+            <?php endif; ?>  
+      </div>
+
+
+
+      <div class="form__row">
+        <label class="form__label" for="project">Проект <sup>*</sup></label>
+
+        <select class="form__input form__input--select <?php if (isset($errors['project'])) echo 'form__input--error';?>" name="project" id="project">
+          
+        
+        
+          
+          <?php
+
+            array_shift($projects);
+            echo '<option value=""> Выберите проект</option>';  
+            
+            foreach ($projects as $project) {
+              print('<option value="'. $project . '"> '. $project . '</option>'); 
+
+                    }
+
+                     
+          ?>
+
+        </select>
+
+        <?php if (isset($errors['project'])): ?> 
+            <p class="p.form__message"><?php echo $errors["project"]?></p>
+            <?php endif; ?>  
+
+      </div>
+
+      <div class="form__row">
+        <label class="form__label" for="date">Дата выполнения</label>
+
+        <input class="form__input form__input--date" type="date" name="date" id="date" value="<?=$task_date;?>" placeholder="Введите дату в формате ДД.ММ.ГГГГ">
+      </div>
+
+      <div class="form__row">
+        <label class="form__label" for="preview">Файл</label>
+
+        <div class="form__input-file">
+          <input class="visually-hidden" type="file" name="preview" id="preview" value="" enctype="multipart/form-data">
+
+          <label class="button button--transparent" for="preview">
+              <span>Выберите файл</span>
+          </label>
+        </div>
+      </div>
+
+      <div class="form__row form__row--controls">
+        <input class="button" type="submit" name="" value="Добавить">
+      </div>
+    </form>
+  </div>
