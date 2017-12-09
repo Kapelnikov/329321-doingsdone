ffc4b7c3e0d5959e3987a56d85c3194e620a0f88
Konstantin Kapelnikov <reg_kapelnikovka@mail.ru>
2017-12-09 19:21:58 +0400

    Задание 15

--- index.php
+++ index.php
@@ -4,6 +4,8 @@ session_start();
 
 require_once(__DIR__.'../config.php');
 require_once(__DIR__.'../functions.php');
+require_once(__DIR__.'../mysql_helper.php');
+require_once(__DIR__.'../init.php');
 require_once(__DIR__.'../data.php');
 require_once(__DIR__.'../userdata.php');
 
